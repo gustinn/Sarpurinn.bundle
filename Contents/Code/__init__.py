d335eb2e496150359146cd15663ab376a7886205
gustinn <agust54@gmail.com>
2017-05-16 19:45:16 +0000

    play test

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -39,7 +39,7 @@ def LiveMenu():
 		title = "Rembrandt",
 		summary = "Skemmtilegt",
 		thumb = R(ICON), #Callback(Thumb, url=thumb),
-		duration = "10s",
+		duration = "3600",
 		#  originally_available_at = date
 		)
 	)
